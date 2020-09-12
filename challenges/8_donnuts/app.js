be8e88772ff4eb60719e0226f8e44d2609bd8a9b
joda <polo629@protonmail.com>
2020-09-11 20:05:20 -0500

    8_donust second part modal width slider done

--- challenges/8_donnuts/app.js
+++ challenges/8_donnuts/app.js
@@ -136,3 +136,60 @@ search.addEventListener("input", () =>
     }
 })
 
+
+// ************* second part -> modal
+
+
+// * click over gallery
+
+let close = document.getElementById("close")
+,   modal = document.querySelector(".modal")
+,   img = document.querySelector(".img_modal")
+,   imgs = ["https://romeojeremiah.github.io/Filter-Project/img/sweets-1.jpeg", "https://romeojeremiah.github.io/Filter-Project/img/sweets-2.jpeg", "https://romeojeremiah.github.io/Filter-Project/img/sweets-3.jpeg", "https://romeojeremiah.github.io/Filter-Project/img/doughnut-1.jpeg", "https://romeojeremiah.github.io/Filter-Project/img/doughnut-2.jpeg","https://romeojeremiah.github.io/Filter-Project/img/doughnut-3.jpeg", "https://romeojeremiah.github.io/Filter-Project/img/cupcake-1.jpeg", "https://romeojeremiah.github.io/Filter-Project/img/cupcake-2.jpeg", "https://romeojeremiah.github.io/Filter-Project/img/cupcake-3.jpeg", "https://romeojeremiah.github.io/Filter-Project/img/cake-1.jpeg", "https://romeojeremiah.github.io/Filter-Project/img/cake-2.jpeg", "https://romeojeremiah.github.io/Filter-Project/img/cake-3.jpeg"]
+,   con = 0
+,   positionImg = null
+
+gallery.addEventListener("click", e =>
+{
+    let target = e.target
+    if(target.matches(".imgCard"))
+    {
+        positionImg = imgs.indexOf(target.src)
+        modal.style.display = "block"
+        img.src = target.src
+        document.body.style.overflow = "hidden"
+    }
+})
+
+
+// * close modal
+
+
+close.addEventListener("click", e =>
+{
+    e.preventDefault()
+    modal.style.display = "none"    
+    document.body.style.overflow = ""
+})
+
+// * left,right arrow
+
+modal.addEventListener("click", e =>
+{
+    let target = e.target
+    if(target.matches(".left"))
+    {
+        e.preventDefault()
+        positionImg--
+        if(positionImg < 0) positionImg = imgs.length - 1
+        img.src = imgs[positionImg]
+
+    }
+    if(target.matches(".right"))
+    {
+        e.preventDefault()
+        positionImg++
+        if(positionImg > imgs.length - 1) positionImg = 0
+        img.src = imgs[positionImg]
+    }
+})
